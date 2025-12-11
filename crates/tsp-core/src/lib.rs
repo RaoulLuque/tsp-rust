385bf852b61e8d6218e92bbafe04da0e0198be25
Raoul Luqué <raoulsluque@gmail.com>
2025-12-11 18:41:21 +0100

    test: Add placeholder tests for all crates

--- crates/tsp-core/src/lib.rs
+++ crates/tsp-core/src/lib.rs
@@ -1,2 +1,10 @@
 pub mod instance;
 pub mod tsp_lib_spec;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
--- crates/tsp-rust/src/main.rs
+++ crates/tsp-rust/src/main.rs
@@ -2,3 +2,11 @@ fn main() {
     let tsp_instance = tsp_parser::parse_tsp_instance("instances/bench/d493.tsp").unwrap();
     println!("Parsed TSP instance: {:?}", tsp_instance.raw_distances());
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
