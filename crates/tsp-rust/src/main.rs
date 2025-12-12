11393f300e5cfab910cebe88ec845acd5723af86
Raoul Luqué <raoulsluque@gmail.com>
2025-12-12 13:27:56 +0100

    perf: Optimize build performance

--- crates/tsp-rust/src/main.rs
+++ crates/tsp-rust/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
-    let tsp_instance = tsp_parser::parse_tsp_instance("instances/bench/d493.tsp").unwrap();
-    println!("Parsed TSP instance: {:?}", tsp_instance.raw_distances());
+    let tsp_instance = tsp_parser::parse_tsp_instance("instances/bench/d18512.tsp").unwrap();
+    // println!("Parsed TSP instance: {:?}", tsp_instance.raw_distances());
 }
 
 #[cfg(test)]
