c0cbfea5b354e0b54af9a4397c2198d9bd71dec7
Raoul Luqué <raoulsluque@gmail.com>
2025-12-26 21:15:16 +0100

    perf: Add benchmark

---
+++ crates/tsp-solvers/benches/held_karp.rs
@@ -0,0 +1,22 @@
+use concorde_rs::solver::tsp_hk;
+use criterion::{Criterion, criterion_group, criterion_main};
+use tsp_parser::parse_tsp_instance;
+
+fn held_karp_benchmark(c: &mut Criterion) {
+    let tsp_instance = parse_tsp_instance("../../instances/bench/berlin52.tsp").unwrap();
+    let lower_distance_matrix = concorde_rs::LowerDistanceMatrix {
+        num_nodes: tsp_instance.metadata().dimension as u32,
+        values: tsp_instance
+            .raw_distances()
+            .iter()
+            .map(|&d| d.0 as u32)
+            .collect(),
+    };
+
+    c.bench_function("Compute min one tree", |b| {
+        b.iter(|| tsp_hk(&lower_distance_matrix).unwrap())
+    });
+}
+
+criterion_group!(held_karp_og, held_karp_benchmark);
+criterion_main!(held_karp_og);
