cc6a8c6cac04e22821198a8fd067f932debc8bf0
Raoul Luqué <raoulsluque@gmail.com>
2025-12-30 15:34:04 +0100

    test: Add macro to easily create benchmarks comparing concorde and own implementation

--- crates/tsp-rust/src/main.rs
+++ crates/tsp-rust/src/main.rs
@@ -2,13 +2,15 @@ use tsp_solvers::held_karp;
 
 fn main() {
     env_logger::init();
-    
+
     let tsp_instance =
-        tsp_parser::parse_tsp_instance("instances/tsplib_symmetric/berlin52.tsp").unwrap();
+        tsp_parser::parse_tsp_instance("instances/tsplib_symmetric/a280.tsp").unwrap();
     // println!("Parsed TSP instance: {:?}", tsp_instance.raw_distances());
     let distances_non_symmetric = tsp_instance.distances().to_non_symmetric();
     let best_tour = held_karp(&distances_non_symmetric);
-    println!("Best tour found: {:?}", best_tour);
+    if let Some(best_tour) = &best_tour {
+        println!("Best tour found: {:?}", best_tour.cost.0);
+    }
 }
 
 #[cfg(test)]
--- crates/tsp-solvers/benches/held_karp.rs
+++ crates/tsp-solvers/benches/held_karp.rs
@@ -3,30 +3,47 @@ use criterion::{Criterion, criterion_group, criterion_main};
 use tsp_parser::parse_tsp_instance;
 use tsp_solvers::held_karp;
 
-fn held_karp_og_12(c: &mut Criterion) {
-    let tsp_instance = parse_tsp_instance("../../instances/tsp_rust/12.tsp").unwrap();
-    let lower_distance_matrix = concorde_rs::LowerDistanceMatrix {
-        num_nodes: tsp_instance.metadata().dimension as u32,
-        values: tsp_instance
-            .raw_distances()
-            .iter()
-            .map(|&d| d.0 as u32)
-            .collect(),
+macro_rules! create_held_karp_benchmarks {
+    ($file_path:expr, $name_concorde:ident, $name_own:ident) => {
+        fn $name_concorde(c: &mut Criterion) {
+            let tsp_instance = parse_tsp_instance(concat!("../../instances/", $file_path)).unwrap();
+            let lower_distance_matrix = concorde_rs::LowerDistanceMatrix {
+                num_nodes: tsp_instance.metadata().dimension as u32,
+                values: tsp_instance
+                    .raw_distances()
+                    .iter()
+                    .map(|&d| d.0 as u32)
+                    .collect(),
+            };
+
+            c.bench_function(concat!("Held Karp using concorde_rs: ", $file_path), |b| {
+                b.iter(|| tsp_hk(&lower_distance_matrix).unwrap())
+            });
+        }
+
+        fn $name_own(c: &mut Criterion) {
+            let tsp_instance = parse_tsp_instance(concat!("../../instances/", $file_path)).unwrap();
+            let non_symmetric_matrix = tsp_instance.distances().to_non_symmetric();
+
+            c.bench_function(
+                concat!("Held Karp using own implementation: ", $file_path),
+                |b| b.iter(|| held_karp(&non_symmetric_matrix).unwrap()),
+            );
+        }
     };
-
-    c.bench_function("Held Karp using concorde_rs", |b| {
-        b.iter(|| tsp_hk(&lower_distance_matrix).unwrap())
-    });
 }
 
-fn held_karp_own_12(c: &mut Criterion) {
-    let tsp_instance = parse_tsp_instance("../../instances/tsplib_symmetric/12.tsp").unwrap();
-    let non_symmetric_matrix = tsp_instance.distances().to_non_symmetric();
+create_held_karp_benchmarks!("tsp_rust/12.tsp", held_karp_concorde_12, held_karp_own_12);
+create_held_karp_benchmarks!(
+    "tsplib_symmetric/berlin52.tsp",
+    held_karp_concorde_berlin52,
+    held_karp_own_berlin52
+);
 
-    c.bench_function("Held Karp using own implementation", |b| {
-        b.iter(|| held_karp(&non_symmetric_matrix).unwrap())
-    });
-}
-
-criterion_group!(held_karp_bench_12, held_karp_og_12, held_karp_own_12);
-criterion_main!(held_karp_bench_12);
+criterion_group!(held_karp_bench_12, held_karp_concorde_12, held_karp_own_12);
+criterion_group!(
+    name = held_karp_bench_berlin52;
+    config = Criterion::default().sample_size(10);
+    targets = held_karp_concorde_berlin52, held_karp_own_berlin52
+);
+criterion_main!(held_karp_bench_12, held_karp_bench_berlin52);
